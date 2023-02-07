3bd107fbbb5cf3963d0014a8badae165d8ec461d
karthigaanandan <87022094+karthigaanandan@users.noreply.github.com>
2023-02-07 19:37:56 +0530

    Update comment.py

--- comment.py
+++ comment.py
@@ -3,4 +3,9 @@ print('This is a single line comment') #This is also a python comment
 
 output: This is a single line comment
 
+  
+# This is a multiline comment in python
+# and expands to more than one line
+print("This is a Multiline  comment")
+output: This is a Multiline  comment
 
